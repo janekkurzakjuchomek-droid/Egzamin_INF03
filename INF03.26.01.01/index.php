5b8187f4110d66abeedd49093b2137292f94af20
janekkurzakjuchomek-droid <janekkurzakjuchomek@gmail.com>
2026-04-15 16:51:05 +0200

    Drugi commit

---
+++ INF03.26.01.01/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <link rel="stylesheet" href="styl.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Konfigurator samochodów</title>
+</head>
+<body>
+    <header>
+        <h1>Serwis konfiguracji samochodów</h1>
+    </header>
+
+    <nav>   
+        <h2>Samochody</h2>
+        <h2>Konfigurator</h2>
+        <h2>Kontakt</h2>
+    </nav>
+
+    <main>
+        <section id="LewaSekcja">  
+            <table>
+                <?php
+                ?>
+            </table>    
+        </section>
+
+        <section id="SrodkowaSekcja">   
+            <table>
+                <th>
+                    <td colspan="1">Konfiguracja</td>
+                    <td></td>
+                    <td>Cena</td>
+                </th>
+                <tr>
+                    <td colspan="2"> <img src="a1.jpg" alt="Konfiguracja 1"> </td>
+                    <td></td>
+                    <td></td>
+                </tr>
+
+                <?php
+                ?>
+
+                <tr>
+                    <td colspan="2"> <img src="a2.jpg" alt="Konfiguracja 2"> </td>
+                    <td></td>
+                </tr>
+
+                <?php
+                ?>
+            </table>    
+        </section>
+
+        <section id="PrawaSekcja">
+             <h3>111 222 444</h3>   
+             <img src="a3.png" alt="Samochód">  
+        </section>
+    </main>
+
+    <footer>  
+        <p>Stronę wykonał: 1235635342</p>    
+    </footer>
+</body>
+</html>
